6381be82790cd8db9e8af3ea0f9a8e4b5c8feb83
pedrosqra <pedro.lima@ccc.ufcg.edu.br>
2021-04-16 17:09:19 -0300

    minor fix

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ const config =
 
 //Connecting to database
 mongoose.connect(
-  `${config}`,
+  `${process.env.DB_CONNECTION}`,
   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
   () => console.log("connected to database")
 );
