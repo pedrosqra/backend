cedca9bf21d97058fbe446158bf67d39fef105cd
pedrosqra <pedro.lima@ccc.ufcg.edu.br>
2021-04-28 23:03:38 -0300

    adding cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const database = require("./config/database");
 require("dotenv/config");
@@ -8,6 +9,7 @@ const routes = require("./routes/routes");
 
 //Middlewares
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 module.exports = app;
